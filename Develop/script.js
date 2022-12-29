d7d25e840f9a2427894a0ad39488aaeae40743dc
abbyd <abbydiaz300@gmail.com>
2022-12-29 12:59:07 -0800

    finished the password generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,64 @@
-// Assignment code here
+const output = document.querySelector('#output')
+const lengthPwd = document.querySelector('#lengthPwd')
+const smallLetter = document.querySelector('#smallLetters')
+const bigLetters = document.querySelector('#bigLetters')
+const numbers = document.querySelector('#numbers')
+const special = document.querySelector('#special')
+const generate = document.querySelector('#generate')
+const copy = document.querySelector('#copy')
 
+const AsmallLettters = ['a','b','c','d','e','f','g','h','i','j','k','l',
+'m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+const AbigLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M',
+'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+const Anumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-  passwordText.value = password;
+const specialCharacters = ['~','!','@','#','$','%','^','&','*','(',')','_',
+'-','=','+','<','>','|',];
 
+generate.onclick = () => output.textContent = generatePwd
+(lengthPwd.value)
+
+copy.onclick = () => {
+  window.navigator.clipboard.writeText(output.textContent)
+  if (window.getSelection) {
+    const sel = window.getSelection();
+    const range = document.createRange();
+    range.selectNodeContents(output)
+    sel.removeAllRanges();
+    sel.addRange(range);
+  }
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function getRandom(array) {
+  const r = Math.floor(Math.random() * array.length)
+  if (array) {
+    return array [r]
+  } else{
+    return ''
+  }
+}
 
-just stestin serjgnsoergjn
+function generatePwd(Len) {
+  let textPwd = '';
+  const activeControls = [
+    smallLetter.checked,
+    bigLetters.checked,
+    numbers.checked,
+    special.checked
+  ].filter (e => e)
+
+  for (let i = 1; i <= Math.round( Len / activeControls.
+  length); i++) {
+    // this is to check that the values are being generated 
+    //console.log(getRandom(AsmallLettters));
+    textPwd += `${smallLetter.checked ? getRandom
+    (AsmallLettters) : ''} ${bigLetters.checked ? getRandom
+      (AbigLetters) : ''} ${numbers.checked ? getRandom
+        (Anumbers) : ''} ${special.checked ? getRandom
+          (specialCharacters) : ''}`
+  }
+  return textPwd
+}
